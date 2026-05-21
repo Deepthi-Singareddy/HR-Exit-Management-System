7d22cdf63e70c8bc6d196d754c257070b76d832c
Deepthi-Singareddy <singareddydeepthi9@gmail.com>
2026-05-21 16:46:48 +0530

    Fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -85,6 +85,8 @@ app.get("/", (req, res) => {
   res.send("Employee Exit System API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
